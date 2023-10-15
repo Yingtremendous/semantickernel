838ca96e31e6db4b1f0689a3fc6875d39b085412
gu <guying005@googlemail.com>
2023-10-15 22:14:50 +0200

    add gitignore

---
+++ sk.py
@@ -0,0 +1,23 @@
+# Configure your local machine to run Semantic Kernel
+import semantic_kernel as sk
+from semantic_kernel.connectors.ai.open_ai import OpenAIChatCompletion, AzureChatCompletion
+"""
+    kernel is for managing resoruces that necessary to run code in 
+    ai application
+    resoruce: confguration, services and pugins 
+"""
+import openai
+import os
+openai.api_key = os.getenv("OPENAI_API_KEY")
+print(openai.api_key)
+kernel = sk.Kernel()
+
+useAzureOpenAI = False
+
+if useAzureOpenAI:
+    deployment, api, endpoint = sk.azure_openai_settings_from_dot_env()
+    kernel.add_text_completion_service("azureioenai", AzureChatCompletion)
+else:
+    api_key, org_id = sk.openai_settings_from_dot_env()
+    kernel.add_text_completion_service("openai", OpenAIChatCompletion("gpt-3.5-turbo-0301", api_key, org_id))
+print("you made a kernel!")
